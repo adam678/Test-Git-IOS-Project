d27fd8e9f1fe6bcc8a42eb70394b6fdca9a7b275
Adam szwarcbard <adamszwarcbard@Adams-MacBook-Pro.local>
2015-06-30 13:06:05 +0300

    add a test class

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Adam szwarcbard on 6/30/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Adam szwarcbard on 6/30/15.
+//
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
